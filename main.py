7456f3ccda8adbee6e2822136892ea8300a3d390
WilliamAughenbaugh <williamrulesftw224@gmail.com>
2021-10-25 03:02:23 -0600

    Files For flask import.

--- main.py
+++ main.py
@@ -1,108 +1,100 @@
-import PySimpleGUI as sg
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.utils import secure_filename
+import panda as pd
 
-import datetime
 
-import pandas as pd
+app = Flask(__name__)
+app.secret_key = "yeet"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.sqlite3'
+app.config['UPLOAD_FOLDER']
+app.config['MAX_CONTENT_PATH'] = 1000000
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-desired_width = 500
-todayDate = datetime.date.today()
-twoWeeksOuts = todayDate + datetime.timedelta(days=14)
-yearVar = todayDate + datetime.timedelta(days=28835)
+db = SQLAlchemy(app)
 
-print(str(yearVar))
+# excel global sheet vars
+file = ""
 
-sg.theme('DarkBlack')
-file_column = [
-    [
-        sg.Text("File Grabber"),
-        sg.In(size=(25, 1), enable_events=True, key="-FILE GRAB-"),
-        sg.FileBrowse(),
-    ],
-    [
-        sg.Multiline(size=(75, 75), key="-LIST ITEMS-"),
-    ]
-]
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customerName = db.Column(db.String, primary_key=True)
+    customerPhoneNumber = db.Column(db.String, primary_key=True)
+    customerProductETA = db.Column(db.String, primary_key=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-new_view_of_file = [
-    [sg.Text("Choose a .xlsx file:")],
-    [sg.Text(key="-TOUT-")],
-    [sg.Text(key="-FILE-")],
-]
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
-layout = [
-    [
-        sg.Column(file_column),
-        sg.HSeparator(),
-        sg.Column(new_view_of_file),
-    ]
-]
-window = sg.Window("Hello", layout, location=(0, 0), size=(1000, 800)).finalize()
 
-while True:
-    event, values = window.read()
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
 
-    if event == "Exit" or event == sg.WIN_CLOSED:
-        break
 
-    if event == "-FILE GRAB-":
-        file = values["-FILE GRAB-"]
-        file = file
-        window["-FILE-"].update(file)
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        file = f.filename
+        return 'file uploaded successfully'
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
         try:
-            workbookGUI = pd.read_excel(file, sheet_name="Sheet1")
-            workbook2GUI = pd.read_excel(file, sheet_name="Sheet1")
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
 
-            customerName = workbookGUI["Customer Name"]
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
-            userName = workbookGUI["Assigned To"]
-            sizeOfArray = len(userName)
-            print(sizeOfArray)
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
 
-            pd.to_datetime(workbook2GUI["ETA"], format="%b %d %Y")
-            eta = workbook2GUI["ETA"]
 
-            customerNameArr = [""]
-            etaArr = [""]
-            etaToCompare: datetime
+def ExcelInfoGrabber():
+    # Global Vars for excel parsing.
+    todayDate = datetime.date.today()
+    twoWeeksOuts = todayDate + datetime.timedelta(days=14)
+    yearVar = todayDate + datetime.timedelta(days=28835)
 
-            for x in range(sizeOfArray):
-                customerNameArr.append(customerName.iloc[0 + x])
-            for x in range(sizeOfArray):
-                etaArr.append(eta.iloc[0 + x])
 
-            for x in range(sizeOfArray):
-                etaStr = etaArr[1 + x]
-                if etaArr[1 + x] == "ERROR":
-                    etaArr[1 + x] = yearVar
-                else:
-                    etaToCompare = etaArr[1 + x]
-                if etaToCompare < twoWeeksOuts:
-                    customerErrStr = str(x) + "Customer " + customerNameArr[
-                        0 + x] + "is showing as an error. Please note down reason for error in the timeline."
-                    if etaStr == "ERROR":
-                        setText = window["-LIST ITEMS-"]
-                        setText.update(setText.get() + "\n" + "\n" + customerErrStr)
-                    else:
-                        withinTwoWeeks = str(x) + " PROACTIVE CONTACT TASK: Called " + customerNameArr[
-                            1 + x] + " to advise of ETA of product for " + str(
-                            etaStr) + " and that we are still on schedule. Advised Independent PROvider will contact them within two business days of when product is available for installation."
-                        if "00:00:00" in withinTwoWeeks:
-                            setText = window["-LIST ITEMS-"]
-                            setText.update(setText.get() + "\n" + "\n" + withinTwoWeeks.replace("00:00:00 ", "")
-                                           + "\n" + "-------------------------------------------------------------------------------------------------")
-                else:
-                    if etaStr == "ERROR":
-                        setText = window["-LIST ITEMS-"]
-                        setText.update(setText.get() + "\n" + "\n" + customerErrStr)
-                    else:
-                        pastTwoWeeks = str(x) + " PROACTIVE CONTACT TASK: Called " + customerNameArr[
-                            0 + x] + " to advise of ETA of product for " + str(
-                            etaStr) + " and that we are still on schedule. Promised follow-up within two weeks. Created follow-up task for two weeks on " + twoWeeksOuts.strftime(
-                            "%b %d %Y") + ". If you are working follow-up task, please contact customer to advise of current ETA or lead time."
-                        if "00:00:00" in pastTwoWeeks:
-                            setText = window["-LIST ITEMS-"]
-                            setText.update(setText.get() + "\n" + "\n" + pastTwoWeeks.replace("00:00:00 ", "")
-                                           + "\n" + "-------------------------------------------------------------------------------------------------")
-        except FileNotFoundError:
-            window["-FILE-"].update("File not found")
-window.close()
+
+    workbookGUI = pd.read_excel(file, sheet_name="Sheet1")
+    workbook2GUI = pd.read_excel(file, sheet_name="Sheet1")
+
+    customerName = workbookGUI["Customer Name"]
+    customerPhoneNum = workbookGUI["Customer Name"]
+    userName = workbookGUI["Assigned To"]
+
+    sizeOfArray = len(userName)
+    print(sizeOfArray)
+
+    pd.to_datetime(workbook2GUI["ETA"], format="%b %d %Y")
+    eta = workbook2GUI["ETA"]
+
+    customerNameArr = [""]
+    customerPhoneNumArr = [""]
+    etaArr = [""]
+    etaToCompare: datetime
+
+    for x in range(sizeOfArray):
+        customerNameArr.append(customerName.iloc[0 + x])
+        customerPhoneNumArr.append(customerPhoneNum.iloc[0+x])
+        for x in range(sizeOfArray):
+            etaArr.append(eta.iloc[0 + x])
+        for x in range(sizeOfArray):
+            etaStr = etaArr[1 + x]
+            if etaArr[1 + x] == "ERROR":
+                etaArr[1 + x] = yearVar
